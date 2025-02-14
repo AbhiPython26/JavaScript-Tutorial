dcd37032d9f24a7c74c392e07242096b13d801ae
Abhishek Gupta <115523268+AbhiPython26@users.noreply.github.com>
2025-02-14 14:00:00 +0000

    ConversionOperations

--- Day_1/ConversionOperations.js
+++ Day_1/ConversionOperations.js
@@ -14,9 +14,9 @@
 // "33abc" => NaN
 // true => 1; false =>0
 
-let isLoggedIn =1
+// let isLoggedIn =1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn)
 
 //1 => true: 0 => false
@@ -24,8 +24,50 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // "hitesh" => true
 
 
-let number = 33
+// let number = 33
 
-let stringNumber = String(number)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let stringNumber = String(number)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+
+// *************************Operations******************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**2)
+// console.log(2%2)
+
+
+// let str1= "Hello"
+// let str2 = "abhishek"
+// let str3 = str1+str2
+// console.log(str3)
+
+// console.log("1" + 2) // => 12
+// console.log(1 + "2") // => 12
+// console.log("1" + 2 + 2)// => 122
+// console.log(1 + 2 + "2")// => 32
+
+
+// console.log(2 * 2 + 5 %6)// this is not good way to write questions.
+
+
+// console.log(true)
+// console.log(+true)
+// console.log(true+)
+// console.log(+"")
+
+
+// let num1, num2 , num3
+// num1 = num2 = num3 = 2+2
+
+let gameCounter=100
+++gameCounter
+console.log(gameCounter)
