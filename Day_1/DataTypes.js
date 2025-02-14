add666056d8411b3741c51d17aee79cc273cec05
Abhishek Gupta <115523268+AbhiPython26@users.noreply.github.com>
2025-02-14 13:22:11 +0000

    DataTypes

--- Day_1/DataTypes.js
+++ Day_1/DataTypes.js
@@ -1,4 +1,23 @@
 "use strict"; // treat all js code as newer version
 
-// alert(3+3) // we are using node js not browser
+//alert(3+3) // we are using node js not browse
 
+console.log(3
+    +
+    3) // this is not a good practice, code readibility should be high
+     console.log("abhishek")
+
+let name = "Abhisshek"
+let age = 21
+let isLoggedIn = false
+let state;
+// number => 2 to power 53
+// bigint => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+// symbol =>unique 
+
+
+//object 
+console.log(typeof "Abhishek")
